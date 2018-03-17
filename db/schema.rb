2e521ab15a62e0ba12fd630db7cde834e12bb14c
theoccultcorner <theoccultcorner@gmail.com>
2018-03-16 20:00:48 -0700

    portfolio items

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180316183350) do
 
   create_table "portfolios", force: :cascade do |t|
     t.string "title"
+    t.string "subtitle"
     t.text "body"
     t.text "main_image"
     t.text "thumb_image"
