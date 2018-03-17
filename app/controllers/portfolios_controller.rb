dea3dab9bb477bc9218ebed2a3f25b0606a9f37b
theoccultcorner <theoccultcorner@gmail.com>
2018-03-17 16:19:33 -0700

    destroy feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -38,4 +38,15 @@ end
   def show
     @portfolio_item = Portfolio.find(params[:id])
  end
+
+ def destroy
+@portfolio_item = Portfolio.find(params[:id])
+
+   @portfolio_item.destroy
+   
+   respond_to do |format|
+     format.html { redirect_to portfolios_url, notice: 'item was successfully destroyed.' }
+
+   end
+ end
 end
